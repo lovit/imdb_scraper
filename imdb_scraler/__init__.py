7815e5d93959b00c13e81a2e2d1c44e6abbb5ba0
lovit <soy.lovit@gmail.com>
2019-01-23 22:15:29 +0900

    Commit utils

--- imdb_scraler/__init__.py
+++ imdb_scraler/__init__.py
@@ -3,3 +3,7 @@ from .credits import parse_credits
 from .keywords import parse_keywords
 from .quotes import parse_quotes
 from .reviews import parse_reviews
+from .utils import now
+from .utils import get_soup
+from .utils import normalize_text
+from .utils import strf_to_datetime
