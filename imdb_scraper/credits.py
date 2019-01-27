6c754bf9ec40adacd33f9f3ab02fd6f3fdc1f91b
lovit <soy.lovit@gmail.com>
2019-01-28 06:39:17 +0900

    Implement parse_credits

--- imdb_scraper/credits.py
+++ imdb_scraper/credits.py
@@ -1,2 +1,50 @@
+from .utils import get_soup
+from .utils import normalize_text
+from .utils import idx_as_strf
+
+credit_base = 'https://www.imdb.com/title/tt{}/fullcredits?ref_=tt_cl_sm#cast'
+
+
 def parse_credits(id):
-    raise NotImplemented
+    """
+    Arguments
+    ---------
+    id : int
+        Movie id
+
+    Returns
+    -------
+    Json format information
+    """
+
+    url = credit_base.format(idx_as_strf(id))
+    soup = get_soup(url)
+
+    marker = '...'
+    credits = []
+
+    trs = soup.select('table[class=cast_list] tr')
+
+    for tr in trs:
+
+        text = tr.text.replace('\n', '').replace('\t','').strip()
+        if not text or not marker in text:
+            continue
+        idx = tr.select('a[href^=/name/nm]')
+        if idx:
+            idx = idx[0].attrs.get('href', '').split('/?')[0]
+            idx = idx.replace('/name/nm','')
+            try:
+                idx = int(idx)
+            except:
+                idx = -1
+        else:
+            idx = -1
+
+        name, role = text.split(marker, 1)
+        name = normalize_text(name)
+        role = normalize_text(role)
+        order= len(credits) + 1
+        credits.append({'name': name, 'id': idx, 'role': role, 'order': order})
+
+    return credits
