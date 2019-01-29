50207b0984cf547d2dc43eb158eb9185ceceff17
lovit <soy.lovit@gmail.com>
2019-01-29 22:22:13 +0900

    Implement parse_keywords

--- imdb_scraper/keywords.py
+++ imdb_scraper/keywords.py
@@ -1,2 +1,25 @@
+from .utils import idx_as_strf
+from .utils import get_soup
+
+
+keyword_base = 'https://www.imdb.com/title/tt{}/keywords?ref_=tt_stry_kw'
+
+
 def parse_keywords(id):
-    raise NotImplemented
+    """
+    Arguments
+    ---------
+    id : int
+        Movie id
+
+    Returns
+    -------
+    List of keywords (str format)
+    """
+
+    id = idx_as_strf(id)
+    url = keyword_base.format(idx)
+    soup = get_soup(url)
+    trs = soup.select('table[class^=dataTable] div[class=sodatext]')
+    keywords = [tr.text.strip() for tr in trs]
+    return keywords
